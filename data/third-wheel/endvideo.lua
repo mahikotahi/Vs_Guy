c63193fe29cc1fc757fcd27d08b0d7ecaf5b2de8
mahikotahi <portilizenseason@gmail.com>
2024-02-03 20:20:48 -0600

    cutscene

---
+++ data/third-wheel/endvideo.lua
@@ -0,0 +1,22 @@
+local allowCountdown = false
+local startedFirstDialogue = false
+local startedEndDialogue = false
+
+function onEndSong()
+    cameraSetTarget("boyfriend")
+    
+    if not allowCountdown and not isStoryMode and not startedFirstDialogue then
+        setProperty('inCutscene', true);
+        runTimer('start', 0.2);
+        startedFirstDialogue = true;
+        return Function_Stop;
+    end
+
+    return Function_Continue;
+end
+function onTimerCompleted(tag, loops, loopsLeft)
+    if tag == 'start' then
+        --startDialogue('dialogueEnd', 'breakfast');
+        startVideo("TW_PostCutscene");
+    end
+end
