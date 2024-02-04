ea334822cb15d8fa7040786b5e3d236828b7d346
mahikotahi <portilizenseason@gmail.com>
2024-02-04 09:20:42 -0600

    xdel wants a fucking song

---
+++ stages/discordm.lua
@@ -0,0 +1,8 @@
+function onCreate()
+
+	-- background shit
+	makeLuaSprite('discordbackground', 'discormmm', 0, 0);
+	scaleLuaSprite("discordbackground", 4.0, 4.0)
+
+	addLuaSprite('discordbackground', false);
+end
